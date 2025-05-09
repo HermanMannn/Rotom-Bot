2e98c1b6d2cd32f37af63902cc24f721f49268b8
HermanMannn <hermannnslol@gmail.com>
2025-05-09 11:56:12 +0400

    Add files via upload

---
+++ DraftGD.js
@@ -0,0 +1,165 @@
+const { SlashCommandBuilder} = require('discord.js');
+const { google } = require('googleapis');
+const SPREADSHEET_ID = '1X0C8o-VuEYJiV_--77MGTAhWrduDdRUOny605MmNHgo';
+const YOUR_CLIENT_ID = '///';
+const YOUR_CLIENT_SECRET = '///';
+const YOUR_REDIRECT_URL ='http://localhost:3000/oauth2callback';
+const YOUR_REFRESH_TOKEN = '///';
+const oauth2Client = new google.auth.OAuth2(
+    YOUR_CLIENT_ID,
+    YOUR_CLIENT_SECRET,
+    YOUR_REDIRECT_URL
+);
+oauth2Client.setCredentials({ refresh_token: YOUR_REFRESH_TOKEN });
+const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gddraft')
+        .setDescription('Draft mon into your team')
+        .addStringOption(option =>
+            option.setName('team')
+            .setDescription('Choose your respective team')
+            .setRequired(true)
+            .addChoices(
+                { name: 'Team 1', value: '1' },
+                { name: 'Team 2', value: '2' },
+                { name: 'Team 3', value: '3' },
+                { name: 'Team 4', value: '4' },
+                { name: 'Team 5', value: '5' },
+                { name: 'Team 6', value: '6' },
+                { name: 'Team 7', value: '7' },
+                { name: 'Team 8', value: '8' },
+                { name: 'Team 9', value: '9' },
+                { name: 'Team 10', value: '10' },
+                { name: 'Team 11', value: '11' },
+                { name: 'Team 12', value: '12' },
+                        )
+        )
+
+        .addStringOption(option => 
+            option.setName('values')
+                .setDescription('Draft your Pokemon')
+                .setRequired(true)),
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+            
+    async execute(interaction) {
+        await interaction.deferReply();
+                    
+        const teamColumns = {
+            '1': 'H',
+            '2': 'U',
+            '3': 'AH',
+            '4': 'AU',
+            '5': 'BH',
+            '6': 'BU',
+            '7': 'H',
+            '8': 'U',
+            '9': 'AH',
+            '10': 'AU',
+            '11': 'BH',
+            '12': 'BU'
+        };
+        const teamValue = interaction.options.getString('team');
+        let x, startRow, endRow, column;
+        if (teamColumns[teamValue]) {
+            column = teamColumns[teamValue];
+            startRow = teamValue <= 6 ? 6 : 27;
+            endRow = teamValue <= 6 ? 17 : 38;
+            x = `Draft Teams!${column}${startRow}:${column}${endRow}`;
+        } else {
+            console.log("Invalid team selection!");
+        }
+       // Get the cell range
+        const valuesInput = interaction.options.getString('values'); // Get the user input 
+        const valuesArray = valuesInput.split(',').map(value => [value]); // Parse values
+        const pointcol = {
+            '1': 'I',
+            '2': 'V',
+            '3': 'AI',
+            '4': 'AV',
+            '5': 'BI',
+            '6': 'BV',
+            '7': 'I',
+            '8': 'V',
+            '9': 'AI',
+            '10': 'AV',
+            '11': 'BI',
+            '12': 'BV'
+        }
+        let y='';
+        if (pointcol[teamValue]) {
+            const colum = pointcol[teamValue];
+            startrow = teamValue <= 6 ? 18 : 39;
+            y = `Draft Teams!${colum}${startrow}`;
+        } else {
+            console.log("Invalid team selection!");
+        }
+        
+        const responses = await Promise.all([
+            sheets.spreadsheets.values.get({ spreadsheetId: SPREADSHEET_ID, range: x }),
+            sheets.spreadsheets.values.get({ spreadsheetId: SPREADSHEET_ID, range: y })
+        ]);
+
+        let points;
+
+        const values = responses[0].data.values;
+        console.log("Mons found in the draft: " + values)
+        points       = responses[1].data.values;
+        console.log("Points read in the beginning: " + points);
+    
+        if(points > 0){
+                for (let i = 0; i < 11; i++) {
+
+                    if(values == undefined || values[i] == undefined){
+                        x = `Draft Teams!${column}${startRow+i}`;
+                        console.log("It was undefined")
+                        break;
+                        
+                    }            
+                    else{
+                        console.log("Continued")
+                        continue;
+                    }
+                }
+            
+
+            console.log(`Drafting to ${x}`);
+        
+            await interaction.editReply("Team found..."); 
+    
+            try {
+                // Update cells 
+                await sheets.spreadsheets.values.update({
+                    spreadsheetId: SPREADSHEET_ID,
+                    range: x, 
+                    valueInputOption: 'USER_ENTERED', 
+                    requestBody: {
+                        values: valuesArray, 
+                    }
+                });
+                const response = await sheets.spreadsheets.values.get({
+                    spreadsheetId: SPREADSHEET_ID,
+                    range: y, 
+                });
+
+                points = response.data.values;
+                points < 0 ? points = points + " contact mod!!!" : points
+
+                await interaction.editReply(`Successfully drafted "${valuesInput}" to Team ${teamValue} you have ${points} points left`);
+                console.log(`Log: Successfully drafted "${valuesInput}" to Team ${teamValue} you have ${points} points left`);
+            } catch (error) {
+                console.error('Error updating Google Sheets:', error);
+                await interaction.editReply('There was an error updating the Google Sheet.');
+            }
+        }
+        else{
+            await interaction.editReply('Your team does not have enough points to draft.');
+        }
+
+    }
+};
